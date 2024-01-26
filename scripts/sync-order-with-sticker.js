93cdc83f6f81be0ac869f00adc2eb41583519c7b
Nasir Mehmood <oknasir@gmail.com>
2024-01-26 16:58:35 +0500

    check order for sticker

--- scripts/sync-order-with-sticker.js
+++ scripts/sync-order-with-sticker.js
@@ -11,33 +11,35 @@ module.exports = {
 
       const _order = await OrderLocal.findOne({ id: summary.orderId });
 
-      const orderObject = JSON.parse(_order.order_object);
+      if (_order) {
+        const orderObject = JSON.parse(_order.order_object);
 
-      const productIds = [];
-      const orderTempId = _order.temp_id;
-      orderObject.forEach((item) => {
-        if (item.id < 1000000) {
-          productIds.push(item.id);
+        const productIds = [];
+        const orderTempId = _order.temp_id;
+        orderObject.forEach((item) => {
+          if (item.id < 1000000) {
+            productIds.push(item.id);
+          }
+        });
+
+        const _update = {
+          orderTempId,
+          orderObject,
+          itemsInOrder: productIds.join(","),
+          productFullId: productIds.join(","),
+          itemsCount: productIds.length.toString(),
+        };
+
+        if (!summary.courierName && summary.cnno) {
+          if (summary.cnno.startsWith("102") || summary.cnno.startsWith("111")) {
+            _update.courierName = "call_courier";
+          } else if (summary.cnno.startsWith("WR")) {
+            _update.courierName = "rider_logistics";
+          }
         }
-      });
 
-      const _update = {
-        orderTempId,
-        orderObject,
-        itemsInOrder: productIds.join(","),
-        productFullId: productIds.join(","),
-        itemsCount: productIds.length.toString(),
-      };
-
-      if (!summary.courierName && summary.cnno) {
-        if (summary.cnno.startsWith("102") || summary.cnno.startsWith("111")) {
-          _update.courierName = "call_courier";
-        } else if (summary.cnno.startsWith("WR")) {
-          _update.courierName = "rider_logistics";
-        }
+        await OrderSummaryLocal.updateOne({ id: summary.id }).set({ ..._update });
       }
-
-      await OrderSummaryLocal.updateOne({ id: summary.id }).set({ ..._update });
 
       return proceed();
     });
